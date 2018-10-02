3a78ceb824f156c2b31ae52985caecbbc8383597
Daniel Felipe Rodríguez Patiño <dfrodriguezp@unal.edu.co>
2018-10-02 11:35:23 -0500

    Ran profile for new version: 25 times faster

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ def main():
     positions, velocities, charges = coldPlasma()
     # positions, velocities, charges = twoStreamUniform()
 
-    folders = ("/phase_space", "/field", "/potential", "/density", "/energy")
-    for i in range(len(folders)):
-        os.system("mkdir -p results{}".format(folders[i]))
+    # folders = ("/phase_space", "/field", "/potential", "/density", "/energy")
+    # for i in range(len(folders)):
+    #     os.system("mkdir -p results{}".format(folders[i]))
 
-    energy = open("results/energy/energy.dat", "w")
+    # energy = open("results/energy/energy.dat", "w")
     
     for step in tqdm(range(steps)):
         nodeIndex = numpy.array(positions / dx, dtype=int)
@@ -36,35 +36,35 @@ def main():
 
         outphase(1.0, finalVelocities, charges, E_p)
 
-        # Write data
-        if step % 10 == 0:
-            phase_space = open("results/phase_space/step_{}.dat".format(step), "w")
-            Efield = open("results/field/step_{}.dat".format(step), "w")
-            potentialOutput = open("results/potential/step_{}.dat".format(step), "w")
-            densityOutput = open("results/density/step_{}.dat".format(step), "w")
+    #     # Write data
+    #     if step % 10 == 0:
+    #         phase_space = open("results/phase_space/step_{}.dat".format(step), "w")
+    #         Efield = open("results/field/step_{}.dat".format(step), "w")
+    #         potentialOutput = open("results/potential/step_{}.dat".format(step), "w")
+    #         densityOutput = open("results/density/step_{}.dat".format(step), "w")
 
-        KE = 0.0
-        FE = 0.0
+    #     KE = 0.0
+    #     FE = 0.0
 
-        for i in range(NG):
-            FE += rho[i] * phi[i]
-            if step % 10 == 0:
-                Efield.write("{} {}\n".format(i*dx, E_n[i]))
-                potentialOutput.write("{} {}\n".format(i*dx, phi[i]))
-                densityOutput.write("{} {}\n".format(i*dx, rho[i]))
+    #     for i in range(NG):
+    #         FE += rho[i] * phi[i]
+    #         if step % 10 == 0:
+    #             Efield.write("{} {}\n".format(i*dx, E_n[i]))
+    #             potentialOutput.write("{} {}\n".format(i*dx, phi[i]))
+    #             densityOutput.write("{} {}\n".format(i*dx, rho[i]))
 
-        for p in range(NP):
-            KE += (charges[p] / numpy.sign(charges[p])) * finalVelocities[p] * finalVelocities[p]
-            if step % 10 == 0:
-                phase_space.write("{} {}\n".format(positions[p], finalVelocities[p]))
+    #     for p in range(NP):
+    #         KE += (charges[p] / numpy.sign(charges[p])) * finalVelocities[p] * finalVelocities[p]
+    #         if step % 10 == 0:
+    #             phase_space.write("{} {}\n".format(positions[p], finalVelocities[p]))
 
-        energy.write("{} {} {}\n".format(KE, FE, step))
-        phase_space.close()
-        Efield.close()
-        densityOutput.close()
-        potentialOutput.close()
+    #     energy.write("{} {} {}\n".format(KE, FE, step))
+    #     phase_space.close()
+    #     Efield.close()
+    #     densityOutput.close()
+    #     potentialOutput.close()
 
-    energy.close()
+    # energy.close()
     
     # rho_test, phi_test, E_n_test = numpy.loadtxt("test/grid_test.txt", unpack=True)
     # pos_test, vel_test, E_p_test = numpy.loadtxt("test/particles_test.txt", unpack=True)
--- params.py
+++ params.py
@@ -1,10 +1,10 @@
 import numpy
 
-NP = 5000
-NG = 101
-steps = 2000
-L = 20.0
+NP = 2048
+NG = 256
+steps = 1000
+L = 4 * numpy.pi
 dx = L / NG
 dt = 0.1
-A = 1e-1
+A = 1e-3
 n = 1
