48b159cfaf02e0b7e2212c95cd636aca971075bb
Daniel Felipe Rodríguez Patiño <dfrodriguezp@unal.edu.co>
2019-05-15 12:49:14 -0500

    Add plotter for positions of particles

---
+++ plotters/plot_space.py
@@ -0,0 +1,71 @@
+from matplotlib import pyplot
+import numpy
+import json
+import os
+import click
+import matplotlib
+matplotlib.style.use('classic')
+
+
+@click.command()
+@click.argument("jsonfile")
+@click.argument("step", default=0)
+def main(jsonfile, step):
+    with open(jsonfile, "r") as json_input:
+        root = json.load(json_input)
+
+    if step >= root["steps"]:
+        print("ERROR! You want to plot a snapshot {} but the limit is {}.".format(
+            step, root["steps"]-1))
+        return
+
+    output = root["output"]
+
+    # Initial data for colors
+    charge = numpy.loadtxt(root["sample"], usecols=(5,))
+
+    colors = numpy.full(len(charge), "r")
+    colors[charge < 0] = "b"
+
+    if "phase_space" in root["results"]:
+        try:
+            x, y, _, _, _ = numpy.loadtxt(
+                "{}/phase_space/step_{}_.dat".format(output, step), unpack=True)
+        except OSError as error:
+            print("ERROR! You want to plot a snapshot that doesn't exist.")
+            return
+    else:
+        try:
+            x, y = numpy.loadtxt(
+                "{}/space/step_{}_.dat".format(output, step), unpack=True)
+        except OSError as error:
+            print("ERROR! You want to plot a snapshot that doesn't exist.")
+            return
+
+    if len(x) > 1e5:
+        x = x[::100]
+        y = y[::100]
+        colors = colors[::100]
+
+    Lx, Ly = root["sys_length"]
+
+    pyplot.figure()
+    pyplot.title("Step {}".format(step), fontsize=25)
+    pyplot.scatter(x, y, c=colors)
+    pyplot.xlabel("$x$", fontsize=25)
+    pyplot.ylabel("$y$", fontsize=25)
+    pyplot.xlim(0, Lx)
+    pyplot.ylim(0, Ly)
+    if Lx == Ly:
+        pyplot.gca().set_aspect("equal")
+    pyplot.grid()
+
+    if "space" in root["results"]:
+        pyplot.savefig("{}/space/step_{}_.pdf".format(output, step))
+    else:
+        os.system("mkdir -p {}/space".format(output))
+        pyplot.savefig("{}/space/step_{}_.pdf".format(output, step))
+
+
+if __name__ == "__main__":
+    main()
