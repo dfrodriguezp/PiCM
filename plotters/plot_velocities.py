9d2878d65cb20f49b87d2d246ea267d3827d6d8f
Daniel Felipe Rodríguez Patiño <dfrodriguezp@unal.edu.co>
2019-05-15 12:52:31 -0500

    Solve bug in velocities plotter

--- plotters/plot_velocities.py
+++ plotters/plot_velocities.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot
 import numpy
 import json
+import os
 import click
 import matplotlib
 matplotlib.style.use('classic')
@@ -41,7 +42,11 @@ def main(jsonfile, step):
     pyplot.hist(vx, bins=50, color="red")
     pyplot.xlabel("$v_x$", fontsize=25)
     pyplot.grid()
-    pyplot.savefig("{}/velocities/step_{}_vx_.pdf".format(output, step))
+    if "velocities" in root["results"]:
+        pyplot.savefig("{}/velocities/step_{}_vx_.pdf".format(output, step))
+    else:
+        os.system("mkdir -p {}/velocities".format(output))
+        pyplot.savefig("{}/velocities/step_{}_vx_.pdf".format(output, step))
 
     # y-component
     pyplot.figure()
@@ -49,7 +54,11 @@ def main(jsonfile, step):
     pyplot.hist(vy, bins=50, color="green")
     pyplot.xlabel("$v_y$", fontsize=25)
     pyplot.grid()
-    pyplot.savefig("{}/velocities/step_{}_vy_.pdf".format(output, step))
+    if "velocities" in root["results"]:
+        pyplot.savefig("{}/velocities/step_{}_vy_.pdf".format(output, step))
+    else:
+        os.system("mkdir -p {}/velocities".format(output))
+        pyplot.savefig("{}/velocities/step_{}_vy_.pdf".format(output, step))
 
     # z-component
     pyplot.figure()
@@ -57,7 +66,11 @@ def main(jsonfile, step):
     pyplot.hist(vz, bins=50, color="blue")
     pyplot.xlabel("$v_z$", fontsize=25)
     pyplot.grid()
-    pyplot.savefig("{}/velocities/step_{}_vz_.pdf".format(output, step))
+    if "velocities" in root["results"]:
+        pyplot.savefig("{}/velocities/step_{}_vz_.pdf".format(output, step))
+    else:
+        os.system("mkdir -p {}/velocities".format(output))
+        pyplot.savefig("{}/velocities/step_{}_vz_.pdf".format(output, step))
 
     # v total
     pyplot.figure()
@@ -65,7 +78,13 @@ def main(jsonfile, step):
     pyplot.hist(vtotal, bins=50, color="yellow")
     pyplot.xlabel(r"$\left|\vec{v}\right|$", fontsize=25)
     pyplot.grid()
-    pyplot.savefig("{}/velocities/step_{}_vtotal_.pdf".format(output, step))
+    if "velocities" in root["results"]:
+        pyplot.savefig(
+            "{}/velocities/step_{}_vtotal_.pdf".format(output, step))
+    else:
+        os.system("mkdir -p {}/velocities".format(output))
+        pyplot.savefig(
+            "{}/velocities/step_{}_vtotal_.pdf".format(output, step))
 
 
 if __name__ == "__main__":
