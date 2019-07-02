d17fb1c8f59cade158ead935c2556df376c9dabc
Daniel Felipe Rodríguez Patiño <dfrodriguezp@unal.edu.co>
2019-07-02 17:38:01 -0500

    Add docstrings to code

--- cycle.py
+++ cycle.py
@@ -2,10 +2,27 @@ import numpy
 
 
 def density(NGx, NGy, dx, dy, hx, hy, currentNodesX, currentNodesY, nxtX, nxtY, indexesInNode, charges):
+    '''Computes particle-to-grid interpolation and returns the values of the charge density on each grid point.
+
+    Parameters:
+        NGx, NGy (int): number of grid points in the x and y dimensions.
+        dx, dy (float): cell size.
+        hx, hy (array): particles relative coordinates on each cell.
+        currentNodesX, currentNodesY (array): origin node for each cell.
+        nxtX, nxtY (array): neighbor nodes of each origin node.
+        indexesinNode (array): particles indexes that are in each cell.
+        charges (array): electric charge of each particle.
+
+    Returns:
+        rho (2D-array): charge density in each node.
+
+    '''
     rho = numpy.zeros(shape=(NGx, NGy))
 
     for node in range(NGx * NGy):
-        i = indexesInNode[node]
+        i = indexesInNode[node] # Get the particles indexes located in the current cell
+
+        # Particle-to-grid interpolation. Add the charge contributions in each node
         rho[currentNodesX[i], currentNodesY[i]
             ] += numpy.sum(charges[i] * (dx - hx[i]) * (dy - hy[i]))
         rho[currentNodesX[i], nxtY[i]
@@ -14,28 +31,44 @@ def density(NGx, NGy, dx, dy, hx, hy, currentNodesX, currentNodesY, nxtX, nxtY, 
             ] += numpy.sum(charges[i] * hx[i] * (dy - hy[i]))
         rho[nxtX[i], nxtY[i]] += numpy.sum(charges[i] * hx[i] * hy[i])
 
+    # Divide for cell size squared to obtain correct dimensions
     rho /= (dx * dy * dx * dy)
 
     return rho
 
 
 def potential(NGx, NGy, dx, dy, rho):
+    '''Returns the values of the electric potential in each grid point
+
+    Parameters:
+        NGx, NGy (int): number of grid points in the x and y dimensions.
+        dx, dy (float): cell size.
+        rho (2D-array): charge density
+    
+    Returns:
+        phi (2D-array): electric potential value in each node.
+    '''
+    
+    # Discrete Fourier transform (DFT) of rho
     rho_k = numpy.fft.fftn(rho)
+
     Wx = numpy.exp(2 * 1j * numpy.pi / NGx)
     Wy = numpy.exp(2 * 1j * numpy.pi / NGy)
-    Wn = 1.0 + 0.0j
     Wm = 1.0 + 0.0j
+    Wn = 1.0 + 0.0j
     dx_2 = dx * dx
     dy_2 = dy * dy
 
-    for n in range(NGx):
-        for m in range(NGy):
-            denom = dy_2 * (2 - Wn - 1.0/Wn) + dx_2 * (2 - Wm - 1.0/Wm)
+    # Find the transform of phi in terms of the transform of rho
+    for m in range(NGx):
+        for n in range(NGy):
+            denom = dy_2 * (2 - Wm - 1.0/Wm) + dx_2 * (2 - Wn - 1.0/Wn)
             if denom != 0:
-                rho_k[n, m] *= dx_2 * dy_2 / denom
-            Wm *= Wy
-        Wn *= Wx
+                rho_k[m, n] *= dx_2 * dy_2 / denom
+            Wn *= Wy
+        Wm *= Wx
 
+    # Compute phi with the real part of the inverse DFT 
     phi = numpy.fft.ifftn(rho_k)
     phi = numpy.real(phi)
 
@@ -43,14 +76,27 @@ def potential(NGx, NGy, dx, dy, rho):
 
 
 def field_n(NGx, NGy, dx, dy, phi):
+    '''Returns the values of the electric field in each grid point
+
+    Parameters:
+        NGx, NGy (int): number of grid points in the x and y dimensions.
+        dx, dy (float): cell size.
+        phi (2D-array): electric potential
+    
+    Returns:
+        E (3D-array): electric field components in each node.
+    '''
     E = numpy.zeros(shape=(NGx, NGy, 3))
+
+    # Compute centered finite difference on x component
     for j in range(NGy):
         for i in range(NGx):
-            nxt_i = (i + 1) % NGx
+            nxt_i = (i + 1) % NGx # Modulo operator for PBC
             prv_i = (i - 1) % NGx
 
             E[i, j, 0] = (phi[prv_i, j] - phi[nxt_i, j]) / (dx * 2)
 
+    # Compute centered finite difference on y component
     for i in range(NGx):
         for j in range(NGy):
             nxt_j = (j + 1) % NGy
@@ -62,44 +108,118 @@ def field_n(NGx, NGy, dx, dy, phi):
 
 
 def field_p(NP, dx, dy, E_n, currentNodesX, currentNodesY, hx, hy, nxtX, nxtY, move_indexes):
+    '''Computes backwards interpolation and returns the value of the electric field on each particle.
+
+    Parameters:
+        NP (int): number of particles.
+        dx, dy (float): cell size.
+        E_n (3D-array): electric field components in each node.
+        currentNodesX, currentNodesY (array): origin node for each cell.
+        hx, hy (array): particles relative coordinates on each cell.
+        nxtX, nxtY (array): neighbor nodes of each origin node.
+        move_indexes (array): indexes of moving particles.
+    
+    Returns:
+        E (2D-array): electric field components on each particle.
+    '''
     E = numpy.zeros(shape=(NP, 3))
 
+    # Weight factors
     A = (dx - hx[move_indexes]) * (dy - hy[move_indexes])
     B = (dx - hx[move_indexes]) * hy[move_indexes]
     C = hx[move_indexes] * (dy - hy[move_indexes])
     D = hx[move_indexes] * hy[move_indexes]
 
+    # Backwards interpolation. Adds the field contribution from the surrounding nodes into each particle
     E[move_indexes, :] += E_n[currentNodesX[move_indexes], currentNodesY[move_indexes], :] * A[:, numpy.newaxis] \
         + E_n[currentNodesX[move_indexes], nxtY[move_indexes], :] * B[:, numpy.newaxis] \
         + E_n[nxtX[move_indexes], currentNodesY[move_indexes], :] * C[:, numpy.newaxis] \
         + E_n[nxtX[move_indexes], nxtY[move_indexes],
               :] * D[:, numpy.newaxis]
 
+    # Divide by cell size to obtain correct dimensions
     E /= (dx * dy)
 
     return E
 
 
 def boris(v1, v2, velocities, QoverM, E_p, dt, move_indexes):
+    '''Updates velocities using the Boris method.
+
+    Parameters:
+        v1, v2 (array): auxiliary vectors of the Boris method.
+        velocities (array): velocity vector of each particle.
+        QoverM (array): q / m ratio of ech particle.
+        E_p (2D-array): electric field acting on each particle.
+        dt (float): time step.
+        move_indexes (array): indexes of moving particles.
+    
+    Returns:
+        None
+    '''
+
+    # Step 1
     v_minus = velocities[move_indexes] + 0.5 * \
         QoverM[move_indexes, numpy.newaxis] * E_p[move_indexes] * dt
+
+    # Step 2
     v_prime = v_minus + numpy.cross(v_minus, v1)
+
+    # Step 3
     v_plus = v_minus + numpy.cross(v_prime, v2)
+
+    # Step 4
     velocities[move_indexes] = v_plus + 0.5 * \
         QoverM[move_indexes, numpy.newaxis] * E_p[move_indexes] * dt
 
 
 def update(v1, v2, positions, velocities, QoverM, E_p, dt, Lx, Ly, move_indexes):
-    boris(v1, v2, velocities, QoverM, E_p, dt, move_indexes)
-    positions += velocities[:, (0, 1)] * dt
+    '''Updates particles position and velocity
 
+    Parameters:
+        v1, v2 (array): auxiliary vectors of the Boris method.
+        velocities (array): velocity vector of each particle.
+        QoverM (array): q / m ratio of ech particle.
+        E_p (2D-array): electric field acting on each particle.
+        dt (float): time step.
+        Lx, Ly (float): length of the system.
+        move_indexes (array): indexes of moving particles.
+    
+    Returns:
+        None
+    '''
+
+    # Update velocities with Boris method
+    boris(v1, v2, velocities, QoverM, E_p, dt, move_indexes)
+
+    # Update position and apply periodic boundary condition
+    positions += velocities[:, (0, 1)] * dt
     positions[:, 0] %= Lx
     positions[:, 1] %= Ly
 
+    # Assert positions are within boundaries
     assert(numpy.all(positions[:, 0] < Lx))
     assert(numpy.all(positions[:, 1] < Ly))
 
 
 def outphase(v1, v2, direction, velocities, QoverM, E_p, dt, move_indexes):
+    '''Performs leapgfrog scheme on velocities
+
+    Parameters:
+        v1, v2 (array): auxiliary vectors of the Boris method.
+        direction (int): forward (+1) or backward (-1) velocity.
+        velocities (array): velocity vector of each particle.
+        QoverM (array): q / m ratio of ech particle.
+        E_p (2D-array): electric field acting on each particle.
+        dt (float): time step.
+        move_indexes (array): indexes of moving particles.
+    
+    Returns:
+        None
+    '''
+
+    # New time outphased by half dt
     dT = 0.5 * direction * dt
+
+    # Update velocities with Boris method
     boris(v1, v2, velocities, QoverM, E_p, dT, move_indexes)
--- main.py
+++ main.py
@@ -7,12 +7,15 @@ import json
 
 
 @click.command()
-@click.argument("jsonfile")
+@click.argument("jsonfile") # Input JSON file as command-line argument
 def main(jsonfile):
+
+    # Make sure JSON file was included
     if not os.path.isfile(jsonfile):
         print("ERROR! Please include a parameters JSON file.")
         return
 
+    # Read JSON file and make sure is well written
     with open(jsonfile, "r") as json_input:
         try:
             root = json.load(json_input)
@@ -25,10 +28,12 @@ def main(jsonfile):
     samplefile = root.get("sample", None)
     outputName = root.get("output", "data")
 
+    # Make sure sample file was given
     if not samplefile:
         print("ERROR! Sample file not found in JSON file.")
         return
 
+    # Make sure sample file exists
     if not os.path.isfile(samplefile):
         print("ERROR! Sample file not found.")
         return
@@ -45,21 +50,29 @@ def main(jsonfile):
     dt = root.get("dt", 0.1)
 
     NP = root.get("N", None)
+
+    # Make sure number of particles was given
     if not NP:
         print("ERROR! Number of particles \"N\" not found in JSON file.")
         return
 
     Bext = numpy.array(root.get("Bfield", [0.0, 0.0, 0.0]))
+
+    # Make sure external magnetic field has three components
     if len(Bext) != 3:
         print("ERROR! Magnetic field must have three components.")
         return
 
     gridSize = root.get("grid_size", [16, 16])
+
+    # Make sure number of nodes has two components
     if len(gridSize) != 2:
         print("ERROR! Grid size must have two components!")
         return
 
     sys_length = root.get("sys_length", [1.0, 1.0])
+
+    # Make sure length of the system has two components
     if len(sys_length) != 2:
         print("ERROR! System length must have two components")
         return
@@ -72,6 +85,7 @@ def main(jsonfile):
 
     sample = root["sample"]
 
+    # Read the sample file and make sure is well written
     try:
         positions = numpy.loadtxt(sample, usecols=(0, 1), unpack=True).T
     except IndexError as error:
@@ -102,7 +116,10 @@ def main(jsonfile):
     charges = Lx * Ly * QoverM / NP
     masses = charges / QoverM
 
+    # Obtain the indexes of the moving particles
     move_indexes, = numpy.where(moves == 1)
+
+    # External magnetic field acting on each particle
     Bext = numpy.full((len(move_indexes), 3), Bext)
 
     folders = ["/energy"]
@@ -119,6 +136,7 @@ def main(jsonfile):
     if writeRho:
         folders.append("/rho")
 
+    # Create folders to store output
     for f in folders:
         os.system("mkdir -p {}".format(outputName + f))
 
@@ -128,6 +146,7 @@ def main(jsonfile):
     # to be confused with time.
 
     v1 = 0.5 * QoverM[move_indexes, numpy.newaxis] * Bext * dt
+
     # Magnitude of v1 squared
     v1_2 = numpy.linalg.norm(v1, axis=1) * numpy.linalg.norm(v1, axis=1)
     v2 = (2 * v1) / (1 + v1_2[:, numpy.newaxis])
@@ -138,38 +157,61 @@ def main(jsonfile):
     print("Simulation running...\n")
     print("Saving data each {} steps.".format(ss_freq))
 
+    # Main cycle
     for step in tqdm(range(steps)):
+        # Whether or not write output for this time step
         writeStep = (step % ss_freq == 0)
+
+        # Origin nodes arrays of each cell that has at least a particle in it.
         currentNodesX = numpy.array(positions[:, 0] / dx, dtype=int)
         currentNodesY = numpy.array(positions[:, 1] / dy, dtype=int)
-        currentX_currentY = currentNodesX + currentNodesY * NGx
 
-        hx = positions[:, 0] - (currentNodesX * dx)
-        hy = positions[:, 1] - (currentNodesY * dy)
+        # Neighbor nodes of the origin nodes of each cell
         nxtX = (currentNodesX + 1) % NGx
         nxtY = (currentNodesY + 1) % NGy
 
+        # Unite both arrays into one
+        currentX_currentY = currentNodesX + currentNodesY * NGx
+
+        # Calculate particles relative position (hx, hy) in each cell
+        hx = positions[:, 0] - (currentNodesX * dx)
+        hy = positions[:, 1] - (currentNodesY * dy)
+
+        # Get the particles indexes that are inside of each cell
         indexesInNode = numpy.array([numpy.where(currentX_currentY == node)[
                                     0] for node in range(NGx * NGy)])
 
+
+        # Step 1: compute rho
         rho = cycle.density(NGx, NGy, dx, dy, hx, hy, currentNodesX,
                             currentNodesY, nxtX, nxtY, indexesInNode, charges)
+
+        # Step 2: from rho, compute phi
         phi = cycle.potential(NGx, NGy, dx, dy, rho)
+
+        # Step 3: from phi, compute electric field on nodes
         E_n = cycle.field_n(NGx, NGy, dx, dy, phi)
+
+        # Backwards interpolation, compute electric field acting on particles
         E_p = cycle.field_p(NP, dx, dy, E_n, currentNodesX,
                             currentNodesY, hx, hy, nxtX, nxtY, move_indexes)
 
+        # Initialize leapfrog scheme, outphase velocities backwards half time step.
         if step == 0:
             cycle.outphase(v1, v2, -1.0, velocities, QoverM,
                            E_p, dt, move_indexes)
 
+        # Step 4: update particles
         cycle.update(v1, v2, positions, velocities, QoverM,
                      E_p, dt, Lx, Ly, move_indexes)
 
         final_velocities = numpy.copy(velocities)
 
+        # Outphase velocities forward for energy calculations and writing output
         cycle.outphase(v1, v2, 1.0, final_velocities,
                        QoverM, E_p, dt, move_indexes)
+
+        # Write output specified in JSON file by user
         if (writeSpace and writeStep):
             space = open("{}/space/step_{}_.dat".format(outputName, step), "w")
             space.write("# x y\n")
@@ -199,6 +241,7 @@ def main(jsonfile):
                 "{}/rho/step_{}_.dat".format(outputName, step), "w")
             chargeDensity.write("# x y rho\n")
 
+        # Compute kinetic and electrostatic energies
         KE = 0.0
         FE = 0.0
 
@@ -213,6 +256,7 @@ def main(jsonfile):
             if (writeVelocities and writeStep):
                 vels.write("{} {} {}\n".format(*final_velocities[p]))
 
+            # Compute kinetic energy
             KE += masses[p] * numpy.linalg.norm(
                 final_velocities[p]) * numpy.linalg.norm(final_velocities[p])
 
@@ -230,12 +274,15 @@ def main(jsonfile):
                     chargeDensity.write("{} {} {} {}\n".format(
                         i * dx, j * dy, rho[i][j]))
 
+                # Compute electrostatic energy
                 FE += rho[i][j] * phi[i][j]
 
         FE *= 0.5
 
+        # Write energy output
         energy.write("{} {} {}\n".format(step, KE, FE))
 
+        # Close output files
         if (writePhaseSpace and writeStep):
             phaseSpace.close()
         if (writeSpace and writeStep):
